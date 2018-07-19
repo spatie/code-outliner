c3c26e7a3fbb82ed924c28151bdc0204e136c8b3
Brent Roose <brent@spatie.be>
2018-07-19 15:40:45 +0200

    Code cleanup

--- src/Commands/OutlineFileCommand.php
+++ src/Commands/OutlineFileCommand.php
@@ -13,7 +13,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
 
 class OutlineFileCommand extends Command
 {
